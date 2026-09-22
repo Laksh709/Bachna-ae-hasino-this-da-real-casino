c21124d0522d914d88a61111072dbf218216d974
Laksh709 <laksh3836@gmail.com>
2026-09-22 18:38:57 +0530

    Seocond variation of the roulette game

--- Roulette.py
+++ Roulette.py
@@ -17,6 +17,7 @@ def play():
     "Another one bites the dust."
 ]
     Bet_type = input("Enter your prefferd type of bet you are wiilling to do : ")
+
     def straight_up():
         print("""
       ___            _     _   _       
@@ -41,8 +42,48 @@ def play():
         else:
             print(random.choice(lose_messages))
             return 0
+    def red_black():
+        print("""
+               ___            _     _   _       
+              | _ \___ _  _  | |___| |_| |_ ___ 
+              |   / _ \ || | | / -_)  _|  _/ -_)
+              |_|_\___/\_,_| |_\___|\__|\__\___|
+             """)
+        
+        red_numbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+        
+          
+        print("--You have choosen Red or Black. Welcome")
+        colour  = input("Enter your choice of colour").strip().capitalize()
+        cash_1 = int(input("Enter the amount you need to bet on "))
+        print("The dealer spins the wheel...")
+        time.sleep(1.5)
+        num_1 = random.randint(0,36)
+        print("The ball is bouncing...")
+        time.sleep(1.5)
+        print(f"It landed on {num_1}!") 
+        if num_1 == 0:
+            color = "Green"
+        elif num_1 in red_numbers:
+            color = "Red" 
+        else:
+            color = "Black"
+        if colour == color:
+            winnings_1 = cash_1*2
+            print(f"Yeahhh,the colour is {color}")
+            print(random.choice(win_messages))
+            print(f"Your price is {winnings_1} $ ")
+            return winnings_1
+        else:
+            print(f"Ohh,the color was {color}")
+            print(random.choice(lose_messages))
+            return 0
+
+
     if Bet_type== "Straight up":
         straight_up()
+    elif Bet_type== "Red or Black":
+        red_black()
     
 play()        
 
